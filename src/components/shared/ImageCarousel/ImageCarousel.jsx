a599e0f79708347387c85c072ea724c7eb0044d2
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-07 18:25:17 +0300

    first deisgn of images carousel

---
+++ src/components/shared/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Slider from 'react-slick';
+import Button from '@mui/material/Button';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Box } from '@mui/material';
+import styles from './ImageCarousel.module.css'; // Import the CSS module
+
+const ImageCarousel = ({ images }) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+    };
+
+    return (
+        <Box className={styles.carouselBox}>
+            <Slider {...settings}>
+                {images.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`Slide ${index}`} className={styles.imageSlide} />
+                    </div>
+                ))}
+            </Slider>
+            <div className={styles.infoOverlay}>
+                <Box className={styles.infoBox}>
+                    <Box marginBottom={7}>
+                        <h2 className={styles.infoHeading}>Carry your Funk</h2>
+                        <p className={styles.infoText}>Trendy handbags collection for your party animal</p>
+                    </Box>
+                    <Button sx={{ backgroundColor: '#1B4B66', color: 'white' }} variant="contained">
+                        See more
+                    </Button>
+                </Box>
+            </div>
+        </Box>
+    );
+};
+
+export default ImageCarousel;
