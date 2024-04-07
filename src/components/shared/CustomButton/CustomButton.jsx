b17f150ed394bea65852844d3ba96cc713e6012f
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-07 20:23:29 +0300

    created custom button compoenent

---
+++ src/components/shared/CustomButton/CustomButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+
+
+const CustomButton = ({ label, icon, onClick }) => {
+    return (
+        <Button sx={{
+            backgroundColor: '#1B4B66', color: 'white',
+            padding: '5px 35px 5px 35px', borderRadius: '8px',
+            width: '180px',
+            maxWidth: '180px',
+            minWidth: '180px',
+            height: '44px',
+            '&:hover': {
+                backgroundColor: '#2B6A8B',
+                color: 'white',
+            },
+        }}
+            variant="contained"
+            startIcon={icon ? <Icon>{icon}</Icon> : undefined}
+            onClick={onClick}
+        >
+            {label}
+        </Button>
+    );
+};
+
+export default CustomButton;
