e7089d1ccd513be14798312f2d5c85c7506bab2d
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-13 18:37:25 +0200

    make primary button responsive

--- src/components/shared/PrimaryButton/PrimaryButton.jsx
+++ src/components/shared/PrimaryButton/PrimaryButton.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Icon from '@mui/material/Icon';
-
+import { useTheme } from '@mui/material/styles';
 
 const PrimaryButton = ({ label, icon, onClick }) => {
+    const theme = useTheme();
+
     return (
         <Button sx={{
-            backgroundColor: '#1B4B66', color: 'white',
-            padding: '5px 35px 5px 35px', borderRadius: '8px',
-            width: '180px',
-            maxWidth: '180px',
-            minWidth: '180px',
+            backgroundColor: theme.palette.primary.main,
+            color: 'white',
+            padding: {
+                xs: '4px 20px',
+                sm: '5px 25px',
+                md: '5px 35px',
+            },
+            borderRadius: '8px',
+            width: {
+                xs: '150px',
+                sm: '160px',
+                md: '180px'
+            },
             height: '44px',
             '&:hover': {
                 backgroundColor: '#2B6A8B',
-                color: 'white',
             },
             textTransform: 'none',
+            fontSize: {
+                xs: '0.875rem',
+                sm: '0.9375rem',
+                md: '1rem',
+            },
+            '& .MuiButton-startIcon': {
+                marginRight: '8px',
+                fontSize: {
+                    xs: '18px',
+                    sm: '20px',
+                    md: '24px',
+                },
+            }
         }}
             variant="contained"
             startIcon={icon ? <Icon>{icon}</Icon> : undefined}
