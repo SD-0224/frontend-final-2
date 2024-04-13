b24a0f1642026687dbc1458ceb362e3445979d6b
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-13 20:18:27 +0200

    Making images Carousel responsive

--- src/components/shared/PrimaryButton/PrimaryButton.jsx
+++ src/components/shared/PrimaryButton/PrimaryButton.jsx
@@ -11,41 +11,44 @@ const PrimaryButton = ({ label, icon, onClick }) => {
             backgroundColor: theme.palette.primary.main,
             color: 'white',
             padding: {
-                xs: '4px 20px',
-                sm: '5px 25px',
+                xs: '2px 10px',
+                sm: '2px 15px',
                 md: '5px 35px',
             },
             borderRadius: '8px',
             width: {
-                xs: '150px',
-                sm: '160px',
+                xs: '70px',
+                sm: '105px',
                 md: '180px'
             },
-            height: '44px',
+            minWidth: {
+                xs: '20px',
+                sm: '40px',
+            },
+            height: {
+                xs: '20px', // Smaller height on extra small screens
+                sm: '30px', // Medium height on small screens, larger screens will use the default height
+                md: '44px'
+            },
+
             '&:hover': {
                 backgroundColor: '#2B6A8B',
             },
             textTransform: 'none',
             fontSize: {
-                xs: '0.875rem',
-                sm: '0.9375rem',
+                xs: '8px',
+                sm: '12px',
                 md: '1rem',
             },
-            '& .MuiButton-startIcon': {
-                marginRight: '8px',
-                fontSize: {
-                    xs: '18px',
-                    sm: '20px',
-                    md: '24px',
-                },
-            }
+
         }}
             variant="contained"
-            startIcon={icon ? <Icon>{icon}</Icon> : undefined}
+            startIcon={icon ? <Icon sx={{ fontSize: { xs: '8px !important', sm: '12px !important', md: '1rem !important' } }}>{icon}</Icon> : undefined
+            }
             onClick={onClick}
         >
             {label}
-        </Button>
+        </Button >
     );
 };
 
