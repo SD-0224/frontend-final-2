6b55c240133b769a62bbb9f9911574dd7dfed00e
Motaz HM <motazhajm@gmail.com>
2024-04-14 03:33:55 +0200

    Add homepage presentation components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { Container, Box } from "@mui/material";
+import ImageCarousel from "../components/shared/ImageCarousel/ImageCarousel";
+import ProductNewArrivals from "../components/product/ProductNewArrivals";
+import ProductHandpicked from "../components/product/ProductHandpicked";
+import ProductBrands from "../components/product/ProductBrands";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "3rem", marginTop: "7rem" }}>
+        <Container>
+          <ImageCarousel images={["/images/slider1.png"]} />
+        </Container>
+        <ProductNewArrivals />
+        <ProductHandpicked />
+        <ProductBrands />
+      </Box>
+    </>
+  );
 };
 
 export default Home;
