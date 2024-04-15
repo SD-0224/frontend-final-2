39c7e975abebd4e745f24e344a75093b725e803b
Motaz HM <motazhajm@gmail.com>
2024-04-15 12:24:06 +0200

    Add product grid presentation component

---
+++ src/components/products/components/ProductGrid.jsx
@@ -0,0 +1,23 @@
+import { List } from "@mui/material";
+import React from "react";
+import ProductCard from "./ProductCard";
+
+const ProductGrid = ({ products }) => {
+  return (
+    <List
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill,minmax(285px,1fr))",
+        columnGap: "2rem",
+        rowGap: "5rem",
+      }}
+    >
+      {products &&
+        products.map((product) => {
+          return <ProductCard product={product} />;
+        })}
+    </List>
+  );
+};
+
+export default ProductGrid;
