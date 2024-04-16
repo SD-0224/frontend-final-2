a2fde193c331c7a94f00a408fbc9a262eb3e6cf4
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-16 20:15:20 +0200

    displaying category with pagination

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { fetchProducts, fetchProductsByCategoryId } from "../../components/products/services/ProductsService";
+import { fetchProductsByCategoryId } from "../../components/products/services/ProductsService";
 import { Container, Box } from "@mui/material";
 import ProductGrid from "../../components/products/components/ProductGrid";
 import AppPagination from "../../components/shared/AppPagination/AppPagination"
 import { useParams } from "react-router-dom";
 import LoadingIndicator from "../../components/shared/LoadingIndicator/LoadingIndicator";
+import { Grid } from "@mui/material";
 
 const Category = () => {
 
@@ -13,7 +14,7 @@ const Category = () => {
     const [loading, setLoading] = useState(false);
     const [productsResult, setProductsResult] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize, setPageSize] = useState(2);
+    const [pageSize, setPageSize] = useState(3);
 
 
     useEffect(() => {
@@ -25,24 +26,30 @@ const Category = () => {
             })
             .catch(error => console.error('Failed to load products:', error));
     }, [currentPage]);
-    useEffect(() => {
-        console.log(productsResult);
-    }, [productsResult]);
 
     return (
         <>
             <Box sx={{ display: "flex", flexDirection: "column", gap: "3rem", margin: "7rem 0" }}>
                 {loading && <LoadingIndicator />}
                 <Container>
-                    <Box>
-                        <ProductGrid products={productsResult.products} />
-                        <AppPagination
-                            currentPage={currentPage}
-                            onPageChange={setCurrentPage}
-                            count={productsResult.count}
-                            pageSize={pageSize}
-                        />
-                    </Box>
+                    <Grid container spacing={2}>
+                        <Grid item xs={2}>
+                            <p>Home  &gt; Handbag</p>
+                            <h2>Category Name</h2>
+                        </Grid>
+
+                        <Grid item xs={10}>
+
+                            <ProductGrid products={productsResult.products} />
+                            <AppPagination
+                                currentPage={currentPage}
+                                onPageChange={setCurrentPage}
+                                count={productsResult.count}
+                                pageSize={pageSize}
+                            />
+                        </Grid>
+
+                    </Grid>
                 </Container>
             </Box>
         </>
