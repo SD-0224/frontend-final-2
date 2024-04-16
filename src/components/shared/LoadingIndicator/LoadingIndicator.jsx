c205312f2153c643dde499413db9272df6afc8b0
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-16 19:44:30 +0200

    created loading indiccator component

---
+++ src/components/shared/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const LoadingIndicator = () => {
+    return (
+        <Box
+            sx={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)'
+            }}
+        >
+            <CircularProgress />
+        </Box>
+    );
+}
+
+export default LoadingIndicator
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -5,19 +5,22 @@ import { Container } from "@mui/material";
 import ProductGrid from "../../components/products/components/ProductGrid";
 import AppPagination from "../../components/shared/AppPagination/AppPagination"
 import { useParams } from "react-router-dom";
+import LoadingIndicator from "../../components/shared/LoadingIndicator/LoadingIndicator";
 
 const Category = () => {
 
     const { categoryId } = useParams();
-
+    const [loading, setLoading] = useState(false);
     const [productsResult, setProductsResult] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [pageSize, setPageSize] = useState(9);
 
 
     useEffect(() => {
+        setLoading(true);
         fetchProductsByCategoryId(categoryId, currentPage, pageSize)
             .then(data => {
+                setLoading(false);
                 setProductsResult(data);
             })
             .catch(error => console.error('Failed to load products:', error));
@@ -26,22 +29,21 @@ const Category = () => {
         console.log(productsResult);
     }, [productsResult]);
 
-
-
-
-
     return (
         <>
-            <Container>
-                <ProductGrid products={productsResult.products} />
-                <AppPagination
-                    currentPage={currentPage}
-                    onPageChange={setCurrentPage}
-                    count={productsResult.count}
-                    pageSize={pageSize}
-
-                />
-            </Container>
+            {loading ? (
+                <LoadingIndicator />
+            ) : (
+                <Container>
+                    <ProductGrid products={productsResult.products} />
+                    <AppPagination
+                        currentPage={currentPage}
+                        onPageChange={setCurrentPage}
+                        count={productsResult.count}
+                        pageSize={pageSize}
+                    />
+                </Container>
+            )}
         </>
     );
 }
