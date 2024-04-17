9cb1af04287dedecbf784cabeeca298bd8713344
Motaz HM <motazhajm@gmail.com>
2024-04-17 21:10:38 +0200

    Refactor Breadcrumb component

--- src/components/shared/Breadcrumb/Breadcrumb.jsx
+++ src/components/shared/Breadcrumb/Breadcrumb.jsx
@@ -7,46 +7,42 @@ import { Link } from "react-router-dom";
 const Breadcrumb = ({ list }) => {
   const theme = useTheme();
   return (
-    <List
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "start",
-        gap: "0.5rem",
-      }}
-    >
-      {list &&
-        list.map((item, index) => {
-          return (
-            <>
-              {index !== list.length - 1 ? (
-                <>
-                  <ListItem key={index} sx={{ width: "max-content", padding: "0" }}>
-                    <Link
-                      style={{
-                        textDecoration: "none",
-                        color: theme.palette.primary.main,
-                        fontWeight: "600",
-                      }}
-                      to={item.link}
-                    >
-                      {item.text}
-                    </Link>
-                  </ListItem>
+    <>
+      <List sx={{ display: "flex", alignItems: "center", justifyContent: "start", gap: "0.5rem" }}>
+        {list &&
+          list.map((item, index) => {
+            return (
+              <React.Fragment key={index}>
+                {index !== list.length - 1 ? (
+                  <>
+                    <ListItem key={index} sx={{ width: "max-content", padding: "0" }}>
+                      <Link
+                        key={`arrow-${index}`}
+                        style={{
+                          textDecoration: "none",
+                          color: theme.palette.primary.main,
+                          fontWeight: "600",
+                        }}
+                        to={item.link}
+                      >
+                        {item.text}
+                      </Link>
+                    </ListItem>
 
+                    <ListItem key={`arrow-${index}`} sx={{ width: "max-content", padding: "0" }}>
+                      <IoIosArrowForward />
+                    </ListItem>
+                  </>
+                ) : (
                   <ListItem key={index} sx={{ width: "max-content", padding: "0" }}>
-                    <IoIosArrowForward />
+                    <ListItemText sx={{ fontWeight: "600" }}>{item.text}</ListItemText>
                   </ListItem>
-                </>
-              ) : (
-                <ListItem key={index} sx={{ width: "max-content", padding: "0" }}>
-                  <ListItemText sx={{ fontWeight: "600" }}>{item.text}</ListItemText>
-                </ListItem>
-              )}
-            </>
-          );
-        })}
-    </List>
+                )}
+              </React.Fragment>
+            );
+          })}
+      </List>
+    </>
   );
 };
 
