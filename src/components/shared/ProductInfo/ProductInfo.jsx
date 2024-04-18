6d9b6010e8d07299e6ab1e90dc4f6d2162e9aeaf
Motaz HM <motazhajm@gmail.com>
2024-04-18 15:06:49 +0200

    Refactor poruct info component to be dynamic and fix pricing discount error

--- src/components/shared/ProductInfo/ProductInfo.jsx
+++ src/components/shared/ProductInfo/ProductInfo.jsx
@@ -1,100 +1,75 @@
-import { Box, Divider, Rating, Stack, Typography } from "@mui/material";
+import { Box, CircularProgress, Divider, Rating, Stack, Typography } from "@mui/material";
 import React from "react";
 import Counter from "../Counter/Counter";
 import { useTheme } from "@mui/material/styles";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
 import SecondaryButton from "../SecondaryButton/SecondaryButton";
 
-const product = {
-  productID: 1,
-  title: "Coach",
-  subTitle: "Leather Coach Bag with adjustable starps.",
-  price: 54.69,
-  discount: 0.5,
-  brandName: "Gucci",
-  category: "Skin Care",
-  avgReview: 4,
-  reviewCount: 250,
-  imgPath: null,
-};
+export default function ProductInfo({ product }) {
+  const { title, subTitle, price, discount, avgReview, reviewsCount } = product;
 
-export default function ProductInfo() {
   const theme = useTheme();
   return (
-    <Box>
-      <Box
+    <Box
+      sx={{
+        [theme.breakpoints.down("sm")]: {
+          maxWidth: "400px",
+        },
+        [theme.breakpoints.up("sm")]: {
+          maxWidth: "600px",
+        },
+        flexGrow: 1,
+      }}
+    >
+      <Typography
+        component={"h1"}
         sx={{
-          [theme.breakpoints.down("sm")]: {
-            maxWidth: "400px",
-          },
-          [theme.breakpoints.up("sm")]: {
-            maxWidth: "600px",
-          },
-          flexGrow: 1,
+          fontWeight: "600",
+          fontSize: "34px",
+          lineHeight: "44px",
+          color: "#13101E",
         }}
       >
+        {title}
+      </Typography>
+      <Typography
+        component={"p"}
+        sx={{
+          fontWeight: "600",
+          fontSize: "20px",
+          lineHeight: "26px",
+          color: "#626262",
+        }}
+      >
+        {subTitle}
+      </Typography>
+      <Stack direction={"row"} alignItems="center" justifyContent="flex-start" margin={"30px 0"}>
+        <Rating name="read-only" value={avgReview} readOnly sx={{ color: "#FF8C4B", marginRight: "10px" }} />
         <Typography
-          component={"h1"}
+          component={"span"}
           sx={{
-            fontWeight: "600",
-            fontSize: "34px",
-            lineHeight: "44px",
-            color: "#13101E",
+            fontWeight: "400",
+            fontSize: "16px",
+            lineHeight: "20px",
+            color: "#B6B6B6",
           }}
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          component={"p"}
-          sx={{
-            fontWeight: "600",
-            fontSize: "20px",
-            lineHeight: "26px",
-            color: "#626262",
-          }}
-        >
-          {product.subTitle}
-        </Typography>
-        <Stack
-          direction={"row"}
-          alignItems="center"
-          justifyContent="flex-start"
-          margin={"30px 0"}
-        >
-          <Rating
-            name="read-only"
-            value={product.avgReview}
-            readOnly
-            sx={{ color: "#FF8C4B", marginRight: "10px" }}
-          />
-          <Typography
-            component={"span"}
-            sx={{
-              fontWeight: "400",
-              fontSize: "16px",
-              lineHeight: "20px",
-              color: "#B6B6B6",
-            }}
-          >{`(${product.reviewCount}) Ratings`}</Typography>
-        </Stack>
-        <Stack
-          direction={"row"}
-          alignItems="center"
-          justifyContent="flex-start"
-        >
-          <Typography
-            component={"h2"}
-            sx={{
-              fontWeight: "700",
-              fontSize: "40px",
-              lineHeight: "52px",
-              color: "#171520",
-              marginRight: "10px",
-            }}
-          >
-            {`$${product.price.toFixed(2)}   `}
-          </Typography>
-          {product.discount ? (
+        >{`(${reviewsCount}) Ratings`}</Typography>
+      </Stack>
+      <Stack direction={"row"} alignItems="center" justifyContent="flex-start">
+        {discount ? (
+          <>
+            <Typography
+              component={"h2"}
+              sx={{
+                fontWeight: "700",
+                fontSize: "40px",
+                lineHeight: "52px",
+                color: "#171520",
+                marginRight: "10px",
+              }}
+            >
+              {`$${(price - discount * price).toFixed(2)}`}
+            </Typography>
             <>
               <Typography
                 component={"h3"}
@@ -107,9 +82,7 @@ export default function ProductInfo() {
                   marginRight: "10px",
                 }}
               >
-                {`${(product.price - product.discount * product.price).toFixed(
-                  2
-                )}`}
+                {`$${price.toFixed(2)}`}
               </Typography>
               <Typography
                 component={"h4"}
@@ -121,30 +94,45 @@ export default function ProductInfo() {
                   marginRight: "10px",
                 }}
               >
-                {`${product.discount * 100}% OFF`}
+                {`${discount * 100}% OFF`}
               </Typography>
             </>
-          ) : null}
-        </Stack>
-        <Divider
-          variant="middle"
-          sx={{
-            marginBottom: "25px",
-            marginTop: "25px",
-          }}
-        />
-        <Stack direction={"row"} sx={{ marginBottom: "50px" }}>
+          </>
+        ) : (
           <Typography
-            component={"p"}
+            component={"h2"}
             sx={{
-              fontWeight: "600",
-              fontSize: "20px",
-              lineHeight: "26px",
-              color: "#13101E",
+              fontWeight: "700",
+              fontSize: "40px",
+              lineHeight: "52px",
+              color: "#171520",
+              marginRight: "10px",
             }}
-          >{`Quantity:`}</Typography>
-          <Counter />
-        </Stack>
+          >
+            {`$${price.toFixed(2)}`}
+          </Typography>
+        )}
+      </Stack>
+      <Divider
+        variant="middle"
+        sx={{
+          marginBottom: "25px",
+          marginTop: "25px",
+        }}
+      />
+      <Stack direction={"row"} sx={{ marginBottom: "50px" }}>
+        <Typography
+          component={"p"}
+          sx={{
+            fontWeight: "600",
+            fontSize: "20px",
+            lineHeight: "26px",
+            color: "#13101E",
+          }}
+        >{`Quantity:`}</Typography>
+        <Counter value={1} />
+      </Stack>
+      <Box sx={{ display: "flex" }}>
         <PrimaryButton label={"Add to bag"} icon={"work"} />
         <SecondaryButton label={"Add To Wishlist"} icon={"favorite"} />
       </Box>
