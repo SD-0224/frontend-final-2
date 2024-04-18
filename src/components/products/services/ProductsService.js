55cf3c1a8f3625b9bda98bdc621209e57b10f631
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-18 12:23:16 +0200

    moved urls to envoirmenent.js , create categories service file, used envoimrent inside product service

--- src/components/products/services/ProductsService.js
+++ src/components/products/services/ProductsService.js
@@ -1,26 +1,23 @@
 import { fetchPath } from "./../../../utilities/fetch"
-// const baseURL = "https://backend-final-2-1.onrender.com";
-
-const mockUrl = "https://fakestoreapi.com/products";
-const baseUrl = "https://backend-final-2-1.onrender.com/products";
+import { environment } from "./../../../utilities/environment"
 
 export const fetchProducts = (pageSize = 9, page = 1) => {
-    return fetchPath(`${baseUrl}?limit=${pageSize}&page=${page}`);
+    return fetchPath(`${environment.baseUrl}/products/search?limit=${pageSize}&page=${page}`);
 };
 
 export const fetchProductsByCategoryId = (categoryId, page = 1, pageSize = 9) => {
-    return fetchPath(`${baseUrl}/category/${categoryId}?page=${page}&limit=${pageSize}`);
+    return fetchPath(`${environment.baseUrl}/products/category/${categoryId}?page=${page}&limit=${pageSize}`);
 };
 
 
 
 export const fetchNewArrivals = () => {
-    return fetchPath(`${mockUrl}?limit=10&sort=desc`);
+    return fetchPath(`${environment.mockUrl}/products?limit=10&sort=desc`);
 };
 
 // Fetch handpicked products
 export const fetchHandpicked = () => {
-    return fetchPath(`${mockUrl}?limit=10`);
+    return fetchPath(`${environment.mockUrl}/products?limit=10`);
 };
 
 
---
+++ src/utilities/environment.js
@@ -0,0 +1,5 @@
+export const environment = {
+    baseUrl: 'https://backend-final-2-1.onrender.com',
+    mockUrl: 'https://fakestoreapi.com'
+};
+
