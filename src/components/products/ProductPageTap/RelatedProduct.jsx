9800e44c1b7fe570c9327beab5854e20420e4e91
Motaz HM <motazhajm@gmail.com>
2024-04-18 15:07:51 +0200

    Refactor related products to reuse horizontal list component

--- src/components/products/ProductPageTap/RelatedProduct.jsx
+++ src/components/products/ProductPageTap/RelatedProduct.jsx
@@ -1,66 +1,23 @@
-import { styled } from "@mui/material/styles";
-import { Typography, Box } from "@mui/material";
-import block from "../../../assets/ProductImg/block.png";
+import { CircularProgress, ListItem } from "@mui/material";
+import HorizontalList from "../../shared/HorizontalList/HorizontalList";
+import ProductCard from "../components/ProductCard";
 
-const ImageWrapper = styled(Box)({
-  width: "80%",
-  height: "40%",
-  "& img": {
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-  },
-});
-
-export default function ProductCard() {
+export default function RelatedProducts({ products }) {
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-        <Box>
-          <ImageWrapper>
-            <img src={block} alt="Description" />
-          </ImageWrapper>
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginLeft: "10px",
-              alignItems: "baseline",
-              flexDirection: "column",
-            }}
-          >
-            <Typography fontSize={"12px"}>title</Typography>
-
-            <Typography fontSize={"12px"} color={"gray"}>
-              subtitle
-            </Typography>
-            <Typography fontSize={"12px"}>$4353</Typography>
-          </Box>
-        </Box>
-        <Box>
-          <ImageWrapper>
-            <img src={block} alt="Description" />
-          </ImageWrapper>
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginLeft: "10px",
-              alignItems: "baseline",
-              flexDirection: "column",
-            }}
-          >
-            <Typography fontSize={"12px"}>title</Typography>
-
-            <Typography fontSize={"12px"} color={"gray"}>
-              subtitle
-            </Typography>
-            <Typography fontSize={"12px"}>$4353</Typography>
-          </Box>
-        </Box>
-      </div>
+      <HorizontalList>
+        {Array.isArray(products) ? (
+          products.map((product, index) => {
+            return (
+              <ListItem key={index}>
+                <ProductCard product={product} />
+              </ListItem>
+            );
+          })
+        ) : (
+          <CircularProgress sx={{ width: "100%", height: "100%", m: "auto" }} />
+        )}
+      </HorizontalList>
     </>
   );
 }
