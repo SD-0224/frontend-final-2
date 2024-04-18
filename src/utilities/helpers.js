f8e8f78f165e22db2ea5d5bf57f69a89a31040cd
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-18 18:46:28 +0200

    refactor slug method to cover all cases

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,4 +1,9 @@
 export const capitalizeSlug = (slug) => {
-    const words = slug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1));
-    return words.join(' ');
+    const words = slug.split('-');
+
+    const capitalizedWords = words.map(word =>
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    );
+
+    return capitalizedWords.join(' ');
 }
