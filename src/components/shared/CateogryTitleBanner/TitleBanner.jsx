8f0e0f3bd2b34aeb1a757f23f6f8e436428ef680
Heba Ali <hebaot.ali@gmail.com>
2024-04-20 16:17:18 +0300

    Adding title banner to category page

--- src/components/shared/CateogryTitleBanner/TitleBanner.jsx
+++ src/components/shared/CateogryTitleBanner/TitleBanner.jsx
@@ -4,7 +4,14 @@ import bannerimg from "../../../assets/TiltleBannerImg/title-banner.svg";
 export default function TitleBaner() {
   return (
     <>
-      <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          width: "100%",
+
+          paddingLeft: { xs: "10px", sm: "15px" },
+          paddingRight: { xs: "10px", sm: "15px" },
+        }}
+      >
         <img src={bannerimg} alt="Hero" style={{ width: "100%" }} />
       </Box>
     </>
