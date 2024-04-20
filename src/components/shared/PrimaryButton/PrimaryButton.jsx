f5ec8aefbf505bdf33bf801e214715a247022077
Motaz HM <motazhajm@gmail.com>
2024-04-20 20:49:32 +0300

    Fix button becoming too small on smaller screens

--- src/components/shared/PrimaryButton/PrimaryButton.jsx
+++ src/components/shared/PrimaryButton/PrimaryButton.jsx
@@ -1,56 +1,32 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Icon from '@mui/material/Icon';
-import { useTheme } from '@mui/material/styles';
+import React from "react";
+import Button from "@mui/material/Button";
+import Icon from "@mui/material/Icon";
+import { useTheme } from "@mui/material/styles";
 
 const PrimaryButton = ({ label, icon, onClick }) => {
-    const theme = useTheme();
-
-    return (
-        <Button sx={{
-            backgroundColor: theme.palette.primary.main,
-            flexGrow: 1,
-            color: 'white',
-            padding: {
-                xs: '2px 10px',
-                sm: '2px 15px',
-                md: '5px 35px',
-            },
-            borderRadius: '8px',
-            width: {
-                xs: '70px',
-                sm: '105px',
-                md: '180px',
-            },
-            minWidth: {
-                xs: '20px',
-                sm: '40px',
-            },
-            height: {
-                xs: '20px', // Smaller height on extra small screens
-                sm: '30px', // Medium height on small screens, larger screens will use the default height
-                md: '44px'
-            },
-
-            '&:hover': {
-                backgroundColor: '#2B6A8B',
-            },
-            textTransform: 'none',
-            fontSize: {
-                xs: '8px',
-                sm: '12px',
-                md: '1rem',
-            },
-
-        }}
-            variant="contained"
-            startIcon={icon ? <Icon sx={{ fontSize: { xs: '8px !important', sm: '12px !important', md: '1rem !important' } }}>{icon}</Icon> : undefined
-            }
-            onClick={onClick}
-        >
-            {label}
-        </Button >
-    );
+  return (
+    <Button
+      sx={{
+        backgroundColor: "main",
+        flexGrow: 1,
+        color: "white",
+        padding: "5px 35px",
+        borderRadius: "8px",
+        minWidth: "fit-content",
+        height: "44px",
+        "&:hover": {
+          backgroundColor: "primary",
+        },
+        textTransform: "none",
+        fontSize: "1rem",
+      }}
+      variant="contained"
+      startIcon={icon ? <Icon sx={{ fontSize: "1rem !important" }}>{icon}</Icon> : undefined}
+      onClick={onClick}
+    >
+      {label}
+    </Button>
+  );
 };
 
 export default PrimaryButton;
--- src/components/shared/SecondaryButton/SecondaryButton.jsx
+++ src/components/shared/SecondaryButton/SecondaryButton.jsx
@@ -10,50 +10,23 @@ const SecondaryButton = ({ label, icon, onClick }) => {
     <Button
       sx={{
         backgroundColor: "#fff",
-        color: theme.palette.primary.main,
+        color: "primary.main",
         flexGrow: 1,
-        padding: "5px 10px",
+        padding: "5px 35px",
         borderRadius: "8px",
-        border: `1px solid ${theme.palette.primary.main}`,
-        width: {
-          xs: "70px",
-          sm: "105px",
-          md: "180px",
-        },
-        minWidth: 'fit-content',
-        height: {
-          xs: "20px", // Smaller height on extra small screens
-          sm: "30px", // Medium height on small screens, larger screens will use the default height
-          md: "44px",
-        },
-        margin:'0 10px',
+        border: `1px solid`,
+        minWidth: "fit-content",
+        height: "44px",
+        margin: "0 10px",
         "&:hover": {
-          backgroundColor: theme.palette.primary.main,
+          backgroundColor: "primary.main",
           color: "#fff",
         },
         textTransform: "none",
-        fontSize: {
-          xs: "8px",
-          sm: "12px",
-          md: "1rem",
-        },
+        fontSize: "1rem",
       }}
       variant="contained"
-      startIcon={
-        icon ? (
-          <Icon
-            sx={{
-              fontSize: {
-                xs: "8px !important",
-                sm: "12px !important",
-                md: "1rem !important",
-              },
-            }}
-          >
-            {icon}
-          </Icon>
-        ) : undefined
-      }
+      startIcon={icon ? <Icon sx={{ fontSize: "1rem !important" }}>{icon}</Icon> : undefined}
       onClick={onClick}
     >
       {label}
