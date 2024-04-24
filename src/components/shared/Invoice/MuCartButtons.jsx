2b7359fecda983ad389086748b41228207750227
Heba Ali <hebaot.ali@gmail.com>
2024-04-24 13:46:11 +0300

    Change buttons label

--- src/components/shared/Invoice/MuCartButtons.jsx
+++ src/components/shared/Invoice/MuCartButtons.jsx
@@ -1,15 +1,24 @@
+import { Padding } from "@mui/icons-material";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
 import SecondaryButton from "../SecondaryButton/SecondaryButton";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, Link } from "@mui/material";
 
 export default function MyCartButtons() {
   return (
     <>
-      <Box sx={{ marginTop: "80px", marginBottom: "20px" }}>
+      <Box
+        sx={{
+          marginTop: "80px",
+          marginBottom: "20px",
+          width: "100%",
+          display: "flex",
+        }}
+      >
         <PrimaryButton
           sx={{ width: "50%", marginRight: "5px" }}
-          label={"Place Order"}
+          label={"GO to checkout"}
         />
+
         <SecondaryButton sx={{ width: "50%" }} label={"Continue Shopping"} />
       </Box>
     </>
