828ad21af4f671be6529cbdc9db1d97c084e36d9
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-24 21:15:07 +0300

    login page login using cookies

--- src/components/authentication/services/AuthenticationService.js
+++ src/components/authentication/services/AuthenticationService.js
@@ -4,25 +4,16 @@ import { environment } from "../../../utilities/environment";
 /**
  *
  * @param {{email: string, password: string}} loginRequest - The login credentials.
- * @returns {Promise<Object>} The login response data.
  */
-export function login(loginRequest) {
-    return fetch(`${environment.baseUrl}/user/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(loginRequest),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Success:", data);
-            return data;
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            throw error;
-        });
+export async function login(loginRequest) {
+    try {
+        const response = await axios.post(`${environment.baseUrl}/user/login`, loginRequest);
+        return response.data;
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
+
 }
 /**
  * Registers a user with given credentials.
