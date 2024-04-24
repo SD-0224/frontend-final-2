a263e003fb73fcdac606e50ded89f1226f70abdb
Motaz HM <motazhajm@gmail.com>
2024-04-24 21:46:42 +0300

    Add cart context provider to routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,71 +13,76 @@ import SignUp from "./pages/Auth/Signup/Signup";
 import WishlistContextProvider from "./context/WishlistContext";
 import MyCart from "./pages/MyCart/MyCart";
 import ProductsList from "./pages/ProductsList/ProductsList";
-
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
     <>
-     <WishlistContextProvider>
       <CategoriesProvider>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            path="/category/:slug"
-            element={
-              <Layout>
-                <Category />
-              </Layout>
-            }
-          />
-          <Route
-            path="/products/:slug"
-            element={
-              <Layout>
-                <ProductDetails />
-              </Layout>
-            }
-          />
-          <Route path="/products/list" element={<Layout><ProductsList /></Layout>}>
-            <Route path="brand/:slug" element={<Outlet />} />
-            <Route path="newest" element={<Outlet />} />
-            <Route path="handpicked/:slug" element={<Outlet />} />
-            <Route path="trendy" element={<Outlet />} />
-            <Route path="discount" element={<Outlet />} />
-          </Route>
-          <Route
-            path="/about"
-            element={
-              <Layout>
-                <About />
-              </Layout>
-            }
-          />
-          <Route
-            path="/mycart"
-            element={
-              <Layout>
-                <MyCart />
-              </Layout>
-            }
-          />
+        <WishlistContextProvider>
+          <CartProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Layout>
+                    <Home />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/category/:slug"
+                element={
+                  <Layout>
+                    <Category />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/products/:slug"
+                element={
+                  <Layout>
+                    <ProductDetails />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/products/list"
+                element={
+                  <Layout>
+                    <ProductsList />
+                  </Layout>
+                }
+              >
+                <Route path="brand/:slug" element={<Outlet />} />
+                <Route path="newest" element={<Outlet />} />
+                <Route path="handpicked/:slug" element={<Outlet />} />
+                <Route path="trendy" element={<Outlet />} />
+                <Route path="discount" element={<Outlet />} />
+              </Route>
+              <Route
+                path="/about"
+                element={
+                  <Layout>
+                    <About />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/mycart"
+                element={
+                  <Layout>
+                    <MyCart />
+                  </Layout>
+                }
+              />
 
-
-
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/signup" element={<SignUp />} />
-        </Routes>
-
-
+              <Route path="/auth/login" element={<Login />} />
+              <Route path="/auth/signup" element={<SignUp />} />
+            </Routes>
+          </CartProvider>
+        </WishlistContextProvider>
       </CategoriesProvider>
-</WishlistContextProvider>
     </>
   );
 }
