7208a92410a13b32c619208c4885881996c763d6
Motaz HM <motazhajm@gmail.com>
2024-04-24 22:23:30 +0300

    Fix duplicate keys issue and remove unnecessary comments

--- src/components/shared/Invoice/Invoice.jsx
+++ src/components/shared/Invoice/Invoice.jsx
@@ -2,16 +2,6 @@ import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import PrimaryButton from "../PrimaryButton/PrimaryButton";
-import SecondaryButton from "../SecondaryButton/SecondaryButton";
-import { Stack, Box } from "@mui/material";
-
-// const OrderDetails = [
-//   { label: "Sub Total", amount: 119.69 },
-//   { label: "Discount", amount: -13.4 },
-//   { label: "Delivery Fee", amount: 0.0 },
-//   { label: "Grand Total", amount: 106.29 },
-// ];
 
 export default function Invoice({ invoiceData }) {
   return (
@@ -25,8 +15,9 @@ export default function Invoice({ invoiceData }) {
         }}
       >
         {invoiceData.map((value, index) => (
-          <ListItem key={value} disableGutters sx={{ padding: "0" }}>
+          <ListItem key={index} disableGutters sx={{ padding: "0" }}>
             <ListItemText
+              key={"label-" + index}
               primary={`${value.label}`}
               primaryTypographyProps={{
                 sx: {
@@ -46,6 +37,7 @@ export default function Invoice({ invoiceData }) {
               }}
             />
             <ListItemText
+              key={"amount-" + index}
               primary={`$${value.amount.toFixed(2)}`}
               primaryTypographyProps={{
                 sx: {
