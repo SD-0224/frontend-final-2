70eb137ffbcd84b5c856aeee81fa865155389107
Mohamed AlQadeery <moh9amad1@gmail.com>
2024-04-25 12:47:34 +0300

    show user links based on authentication state

--- src/components/shared/Popup/UserPopupLinks.jsx
+++ src/components/shared/Popup/UserPopupLinks.jsx
@@ -1,40 +1,56 @@
 import React from 'react'
 import { Stack, Link, useTheme } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import { useAuthenticatedUserContext } from '../../../context/AuthenticatedUser';
-import SecondaryButton from '../SecondaryButton/SecondaryButton';
+import { useAuthenticatedUserContext } from '../../../context/AuthenticatedUserContext';
+import { useUserPopupContext } from '../../../context/UserPopupContext';
 
 export default function UserPopupLinks() {
   const { isAuthenticated } = useAuthenticatedUserContext();
-  return isAuthenticated ? (
-    <Typography
-      variant="subtitle1"
-      sx={{
-        padding: "20px 0px",
-        textAlign: "center",
-        fontSize: "24px",
-        color: "textSecondary",
-      }}
-    >
-      Hello ! !
-    </Typography>
-  ) : (
-    <Stack sx={{ listStyleType: "none", padding: "0px", margin: "0px" }}>
-      <Link component={RouterLink} to="/auth/login" sx={{
-        textDecoration: "none ",
-        paddingBlock: "10px",
-        ":hover": { backgroundColor: "#f5f5f5" }
-      }}>Login</Link>
-      <Link component={RouterLink} to="/auth/signup" sx={{
-        textDecoration: "none ",
-        paddingBlock: "10px",
-        ":hover": { backgroundColor: "#f5f5f5" }
-      }}>Signup</Link>
-      <Link onClick={() => console.log("logout")} sx={{
-        textDecoration: "none ",
-        paddingBlock: "10px",
-        ":hover": { backgroundColor: "#f5f5f5", cursor: "pointer" }
-      }}>Logout</Link>
-    </Stack>
-  );
+  const { closeUserPopup } = useUserPopupContext();
+
+  return (
+    <>
+      <Stack sx={{ listStyleType: "none", padding: "0px", margin: "0px" }}>
+        {!isAuthenticated &&
+          <>
+            <Link component={RouterLink} to="/auth/login" onClick={() => closeUserPopup()} sx={{
+              textDecoration: "none ",
+              paddingBlock: "10px",
+              ":hover": { backgroundColor: "#f5f5f5" }
+            }}>Login</Link>
+            <Link component={RouterLink} to="/auth/signup" onClick={() => closeUserPopup()} sx={{
+              textDecoration: "none ",
+              paddingBlock: "10px",
+              ":hover": { backgroundColor: "#f5f5f5" }
+            }}>Signup</Link>
+          </>
+        }
+
+        {isAuthenticated &&
+
+          <>
+            <Link component={RouterLink} to="/" sx={{
+              textDecoration: "none ",
+              paddingBlock: "10px",
+              ":hover": { backgroundColor: "#f5f5f5" }
+            }}
+
+              onClick={() => closeUserPopup()}
+            >Profile </Link>
+            <Link onClick={() => {
+              console.log("Logout");
+              closeUserPopup();
+            }} sx={{
+              textDecoration: "none ",
+              paddingBlock: "10px",
+              ":hover": { backgroundColor: "#f5f5f5", cursor: "pointer" }
+            }}
+            >Logout</Link>
+          </>
+        }
+      </Stack>
+
+    </>
+  )
+
 }
