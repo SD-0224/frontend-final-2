3f25d3314ba15a1fc4ab44582cee5b750dc3d827
Hamada Assi <1181121@student.birzeit.edu>
2024-04-29 12:40:18 +0300

    Make the product details tab more reusable and dynamic data

---
+++ src/components/products/ProductPageTap/ProductDescription.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export default function ProductDescription({ description }) {
+  return (
+    <Typography
+      sx={{
+        whiteSpace: "pre-wrap",
+        fontSize: {
+          xs: "13px",
+          sm: "16px",
+        },
+      }}
+    >
+      {description}
+    </Typography>
+  );
+}
