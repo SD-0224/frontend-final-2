382113865301e3c621809cd22eea2b41892fc268
Hamada Assi <1181121@student.birzeit.edu>
2024-04-30 14:37:18 +0300

    add my address book section design

---
+++ src/components/myAddressBook/MyAddressBookSection.jsx
@@ -0,0 +1,65 @@
+import { Box, Grid } from "@mui/material";
+import React from "react";
+import AddressDetails from "./AddressDetails";
+import OrderSummary from "../shared/OrderSummary/OrderSummary";
+
+export default function MyAddressBookSection() {
+  const addresses = [
+    {
+      addressID: 1,
+      userID: 21,
+      street: "123 Main St",
+      state: "California",
+      city: "Los Angeles",
+      postalCode: "90001",
+    },
+    {
+      addressID: 2,
+      userID: 21,
+      street: "123 Main St",
+      state: "California",
+      city: "Los Angeles",
+      postalCode: "90001",
+    },
+    {
+      addressID: 3,
+      userID: 21,
+      street: "123 Main St",
+      state: "California",
+      city: "Los Angeles",
+      postalCode: "90001",
+    },
+    {
+      addressID: 4,
+      userID: 21,
+      street: "123 Main St",
+      state: "California",
+      city: "Los Angeles",
+      postalCode: "90001",
+    },
+  ];
+  return (
+    <OrderSummary title={"My Address"}>
+      <Grid container spacing={3} paddingTop={3}>
+        {addresses.map((address, index) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <Box
+              sx={{
+                padding: "20px 20px 40px",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                border: "1px solid #ccc",
+                borderRadius: "8px",
+                transition: "box-shadow 0.3s ease-in-out",
+                "&:hover": {
+                  boxShadow: "0px 0px 5px 3px rgba(0, 0, 0, 0.1)",
+                },
+              }}
+            >
+              <AddressDetails address={address} />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </OrderSummary>
+  );
+}
